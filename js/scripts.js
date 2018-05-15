d921f7b79d4142442db42b0751f6e065fdcd0160
lauraayushi <royalsayushi@gmail.com>
2018-05-15 17:25:12 +0300

    added prototypes

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,40 @@
 //business-logic
 //before the game starts
-function(number) {
-  this.number = number;
+function Playername(playername) {
+  this.playername = playername;
+  this.score = 0;
+};
+//before initializing of the random number
+function Playersturn(playerturn) {
+  this.score = 0;
+  this.randomnumber = 0;
 }
-
-function(points) {
-  this.points += points;
-}
-score = 0
-//during the first turn of the pigdice game
+//during intialization of the random number
 var rollDice = Mathfloor(Math.random()*6 + 1);
 if(rollDice === 1) {
-   this.points =0;
+   this.score =0;
+   alert("You have scored one.It is now player2 turns")
 }else{
-   this.points += rollDice;   
+   this.score += rollDice;   
 }
 return rollDice;
 // during the next turns of the pigdice game
-var secondTurn
+player1.prototype.rollDice= function () {
+return rollDice;
+}
+player2.prototype.rollDice = function () {
+return rollDice;
+}
+// user-interface
+	$(document).ready(function() {
+	 $("#former").submit(function(event){
+	  $("#outcome").empty();
+	   var resultNumber = parseInt($("input#input-1").val());
+	   var outputResults =rollDice;
+
+	 outputResults.forEach(function(item){
+	$("#outcome").append('<li>'+ item + '</li>');
+	});
+	event.preventDefault();
+   });
+  });
